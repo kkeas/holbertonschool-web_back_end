49308d435b3a05149cd94f01dbd8c639b82053b7
katrina keas <katrinakeas@katrinas-MacBook-Pro.local>
2023-05-12 21:45:00 -0500

    try again

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -8,7 +8,5 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API')
       resolve(data);
   } else {
-    reject(new Error);
-  }
-  }) 
-};
+    reject(new Error());
+  }})};
