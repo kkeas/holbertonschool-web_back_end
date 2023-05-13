24c1c242aa852a90ace6c0bfbe84e0a5b3cbb28c
katrina keas <katrinakeas@katrinas-MacBook-Pro.local>
2023-05-12 21:41:23 -0500

    made some format changes

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      const data = {
+        status: 200,
+        body: 'Success',
+      }
+      console.log('Got a response from the API')
+      resolve(data);
+  } else {
+    reject(new Error);
+  }
+  }) 
+};
