bf0f165ea447f99e77dff2353de750a8c25cb198
katrina keas <katrinakeas@katrinas-MacBook-Pro.local>
2023-05-12 23:06:20 -0500

    finally

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,12 @@
-export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      const data = {
-        status: 200,
-        body: 'Success',
-      }
-      console.log('Got a response from the API')
-      resolve(data).then((data) => {
-        console.log(data);
-      });
-  } else {
-    reject(new Error());
-  }})};
+/* eslint-disable no-unused-vars */
+export default function handleResponseFromApi(promise) {
+  return promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch((error) => {
+      console.error(error);
+      return new Error();
+    });
+}
