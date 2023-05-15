0887870b6aeeac50611de3f3b830adcb975f9484
katrina keas <katrinakeas@katrinas-MacBook-Pro.local>
2023-05-15 11:42:10 -0500

    filename

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,4 +1,4 @@
 export default function uploadPhoto(filename) {
-  return Promise.reject(new Error(`${fileName} cannot be processed`));
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 
 }
