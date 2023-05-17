17cfbbf4b16b9dc3284982c68ab04a0c20ecf0da
katrina keas <katrinakeas@katrinas-MacBook-Pro.local>
2023-05-17 16:05:22 -0500

    i dont know

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,12 +1,6 @@
+import ClassRoom from './0-classroom';
 
-const ClassRoom = require('./0-classroom.js');
-
-function initializeRooms() {
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
-  ];
-
-  return rooms;
+export default function initializeRooms() {
+  const sizes = [19, 20, 34];
+  return sizes.map((size) => new ClassRoom(size));
 }
