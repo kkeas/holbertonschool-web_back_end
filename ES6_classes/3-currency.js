d9c34a0f9ff2b399e431152f6f5e06c080e9fe9a
katrina keas <katrinakeas@katrinas-MacBook-Pro.local>
2023-05-17 16:49:39 -0500

    got this

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,37 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+
+const currency = new Currency('USD', 'US Dollar');
+console.log(currency.displayFullCurrency());
