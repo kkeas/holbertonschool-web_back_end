f5b6787f410f8e576613af4595f5e30966199d14
katrina keas <katrinakeas@katrinas-MacBook-Pro.local>
2023-05-17 16:02:54 -0500

    easy enough

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,15 @@
+
+const ClassRoom = require('./0-classroom.js');
+
+function initializeRooms() {
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ];
+
+  return rooms;
+}
+
+const classrooms = initializeRooms();
+console.log(classrooms);
