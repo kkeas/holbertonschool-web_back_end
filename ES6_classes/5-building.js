3cc6b08123f4dd57c93558e33effe303e6e0015b
katrina keas <katrinakeas@katrinas-MacBook-Pro.local>
2023-05-17 19:06:58 -0500

    excavation warning msg

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  /* eslint-disable class-methods-use-this */
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
